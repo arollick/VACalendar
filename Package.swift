fdca5bba37fec370a0c6f07a37093a55ec0b3402
arollick <arollick@profit.com>
2025-06-28 12:58:09 +0200

    Add SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "VACalendar",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "VACalendar", targets: ["VACalendar"])
+    ],
+    targets: [
+        .target(
+            name: "VACalendar",
+            path: "Sources/VACalendar"
+        )
+    ]
+)
